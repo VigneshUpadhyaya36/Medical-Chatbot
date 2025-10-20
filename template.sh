bc36927f62c93084926833155b19926fe7d8b807
Vignesh Upadhyaya <upadhyayavignesh36@gmail.com>
2025-10-20 15:36:22 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,18 @@
+
+#creating directory
+mkdir -p src
+mkdir -p src research
+
+#create file
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+
+
+echo "Done"
